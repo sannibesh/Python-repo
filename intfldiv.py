0470fd2770efe2d474d21a34f004714b1a5bcd5d
sannibesh <sannibesh33@gmail.com>
2025-01-03 14:17:18 +0530

    added

---
+++ intfldiv.py
@@ -0,0 +1,7 @@
+#int div with float
+a=45
+b=7.0
+intfldiv=a//b
+print(intfldiv)
+#or
+print(a//b)
---
+++ moduls.py
@@ -0,0 +1,15 @@
+a=13
+b=4
+print(a%b)
+#or
+c= -17
+d= -2
+print(c%d)
+#or
+e=17
+f=-4
+print(e%f)
+#or
+g=-19
+h=6
+print(g%h)
