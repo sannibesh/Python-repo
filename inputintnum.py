b140f1ec683d7a750f405154719e262ddb92a3cd
sannibesh <sannibesh33@gmail.com>
2025-01-04 02:51:48 +0530

    added

---
+++ inputintnum.py
@@ -0,0 +1,5 @@
+num1=input("enter a number: ")
+num2=input("enter a number: ")
+num1=int(num1)
+num2=int(num2)
+print("result: ", num1+num2)
---
+++ inputstrnum.py
@@ -0,0 +1,3 @@
+num1=input("enter a number: ")
+num2=input("enter a number: ")
+print("result: ", num1+num2)
