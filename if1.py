b356147468d371e7ab0a43c12e067e84b7fd18ef
sannibesh <sannibesh33@gmail.com>
2025-01-04 19:29:08 +0530

    added

---
+++ if1.py
@@ -0,0 +1,4 @@
+A33student=['Abhijit', 'Kushuk', 'Mrinmoy', 'Amit', 'Soumya']
+student= input('enter the name of the student of A33: ')
+if student in A33student:
+    print(student, " is in A33")
---
+++ ifelse.py
@@ -0,0 +1,6 @@
+BallonDor=['Messi','Pele','Ronaldo','Cruyff', 'Platini', 'Maradona', 'Zidan','Bakenbower', 'Maldini', 'Baresi']
+player=input('Enter the name of a best player: ')
+if player in BallonDor:
+    print(player,' is the best')
+else:
+    print('sorry, he is not counted as the best')
