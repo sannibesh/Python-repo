5948f958167ab3c7d9919afa5ebbf0cdab25c3d7
sannibesh <sannibesh33@gmail.com>
2025-01-05 02:43:35 +0530

    added

---
+++ leap2.py
@@ -0,0 +1,13 @@
+year=input("enter the year: ")
+year=int(year)
+
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("It is a leap year")
+        else:
+            print("It is not a leap year")
+    else:
+        print("It is a leap year")
+else:
+    print("It is not a leap year")
