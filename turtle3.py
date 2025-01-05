8f59d36a3892c1a2ff587eca36d70b4d7af96c93
sannibesh <sannibesh33@gmail.com>
2025-01-05 23:09:48 +0530

    turtle game triagle

---
+++ turtle3.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(1)
+
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+
+turtle.exitonclick()
